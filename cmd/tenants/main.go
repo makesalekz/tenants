ab7301c1bb113b8e1a53d78e6b25da906facd66b
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2023-12-28 11:55:49 +0600

    Fix server reflection

--- cmd/tenants/main.go
+++ cmd/tenants/main.go
@@ -22,7 +22,7 @@ var (
 	// Name is the name of the compiled software.
 	Name string = "tenants"
 	// Version is the version of the compiled software.
-	Version string = "0.1.2"
+	Version string = "0.1.3"
 	// flagconf is the config flag.
 	flagconf string
 
